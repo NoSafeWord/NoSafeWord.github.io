f5e02fb27de7106a0b04e6b202d3b735e3b21931
Johnatan Moe Lester <lolaloveyou33@gmail.com>
2023-10-03 17:18:11 +0000

    Update test.user.js

--- test.user.js
+++ test.user.js
@@ -1,1 +1,10 @@
-alert(1)
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://hu94.grepolis.com/game/index?login=1&p=187897&ts=1696327547
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=grepolis.com
+// @grant        none
+// ==/UserScript==
